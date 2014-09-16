45990e0f8b319db6b8ccf2ce7c2c9138aa464c51
instruktor <instruktor@10.12.19.126>
2014-09-16 11:34:52 +0200

    pierwsza encja

---
+++ logistic-core/src/main/java/pl/altkom/logistic/core/model/Car.java
@@ -0,0 +1,51 @@
+package pl.altkom.logistic.core.model;
+
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author instruktor
+ */
+@Entity
+@SuppressWarnings("PersistenceUnitPresent")
+public class Car implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Size(max = 128)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    
+}
---
+++ logistic-core/src/test/java/pl/altkom/logistic/core/TestContext.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.altkom.logistic.core;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+/**
+ *
+ * @author instruktor
+ */
+@ContextConfiguration("/core-test-context.xml")
+@TransactionConfiguration(defaultRollback = false)
+public class TestContext extends AbstractTransactionalJUnit4SpringContextTests {
+    
+    @Test
+    public void test() {
+        System.out.println("11");
+    }
+    
+}
