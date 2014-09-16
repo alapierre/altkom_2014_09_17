27da7f260fe45d756f0357ecfa490f8a879f3b80
kursant6 <kursant6@kursant6-pc.319.wroclaw.altkom.local>
2014-09-16 14:58:08 +0200

    Added InvoiceItem entity.

--- logistic-core/src/main/java/pl/altkom/logistic/core/model/InvoiceItem.java
+++ logistic-core/src/main/java/pl/altkom/logistic/core/model/InvoiceItem.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package pl.altkom.logistic.core.model;
 
 import javax.persistence.Entity;
@@ -12,18 +13,6 @@ import javax.persistence.Entity;
  * @author Pawel Beder <AA@aa.com>
  */
 @Entity
-@SuppressWarnings("PersistenceUnitPresent")
-public class TaxRate extends BaseEntity {
-
-    private int rate;
-    private InvoiceItem item;
-
-    public int getRate() {
-        return rate;
-    }
-
-    public void setRate(int rate) {
-        this.rate = rate;
-    }
-
+class InvoiceItem extends BaseEntity {
+    
 }
