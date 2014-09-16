00ecbd17de2ad507c274fb42dac2c68ac75524fc
bartek <kursant7@kursant7-pc.319.wroclaw.altkom.local>
2014-09-16 12:44:14 +0200

    Merge bartek

--- logistic-core/src/test/java/pl/altkom/logistic/core/TestContext.java
+++ logistic-core/src/test/java/pl/altkom/logistic/core/TestContext.java
@@ -6,7 +6,6 @@
 
 package pl.altkom.logistic.core;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
