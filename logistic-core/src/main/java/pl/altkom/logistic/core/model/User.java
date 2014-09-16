7a44e446ba4f1e6ec7c589b107e69c88567f3aa7
kursant8 <kursant8@kursant8-pc.319.wroclaw.altkom.local>
2014-09-16 15:40:19 +0200

    Merge origin/master
    Conflicts:
    	logistic-core/src/main/java/pl/altkom/logistic/core/model/User.java

--- logistic-core/src/main/java/pl/altkom/logistic/core/model/User.java
+++ logistic-core/src/main/java/pl/altkom/logistic/core/model/User.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 /*
