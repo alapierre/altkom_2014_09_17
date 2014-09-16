82ea7fcf03517b2a8a00e4e02c5b72e0d249d160
kursant1 <kursant1@kursant1-pc.319.wroclaw.altkom.local>
2014-09-16 15:25:03 +0200

    user 2

--- logistic-core/src/main/java/pl/altkom/logistic/core/model/User.java
+++ logistic-core/src/main/java/pl/altkom/logistic/core/model/User.java
@@ -7,12 +7,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author lukasz s
