c3329b1cb599bfcfa5db13a236957d7ea271cf34
kursant6 <kursant6@kursant6-pc.319.wroclaw.altkom.local>
2014-09-17 10:13:24 +0200

    TaxRateDAO + tests + java 8 compiler on logistic-core

--- logistic-core/src/test/java/pl/altkom/logistic/core/TestSpringData.java
+++ logistic-core/src/test/java/pl/altkom/logistic/core/TestSpringData.java
@@ -3,17 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pl.altkom.logistic.core;
-import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import pl.altkom.logistic.core.model.Car;
+import pl.altkom.logistic.core.model.TaxRate;
 import pl.altkom.logistic.dao.springdata.CarDAO;
-
+import pl.altkom.logistic.dao.springdata.TaxRateDAO;
 
 /**
  *
@@ -22,23 +22,26 @@ import pl.altkom.logistic.dao.springdata.CarDAO;
 @ContextConfiguration("/core-test-context.xml")
 @TransactionConfiguration(defaultRollback = false)
 public class TestSpringData extends AbstractTransactionalJUnit4SpringContextTests {
-    
+
     @Autowired
     private CarDAO carDAO;
-    
+
+    @Autowired
+    private TaxRateDAO taxRateDAO;
+
     @Test
     public void test() {
         System.out.println("11");
     }
-    
+
     @Test
     public void testLoad() {
         Car car = carDAO.findOne(1);
         System.out.println("car " + car);
     }
-    
+
     @Test
-    public void createCar(){
+    public void createCar() {
         String carName = "Zaporozec";
         Car car = new Car();
         car.setName(carName);
@@ -46,22 +49,51 @@ public class TestSpringData extends AbstractTransactionalJUnit4SpringContextTest
         //Car foundCar = carDAO.findByName(carName);
         //assertNotNull(foundCar);
     }
+
+    @Test
+    public void createTaxRate() {
+        TaxRate tr = new TaxRate();
+        tr.setRate(18);
+        taxRateDAO.save(tr);
+        tr = new TaxRate();
+        tr.setRate(22);
+        taxRateDAO.save(tr);
+        tr=new TaxRate();
+        tr.setRate(22);
+        taxRateDAO.save(tr);
+        tr = new TaxRate();
+        tr.setRate(27);
+        taxRateDAO.save(tr);
+    }
+
+    @Test
+    public void testFindById() {
+        Iterable<TaxRate> findById = taxRateDAO.findById(2);
+        System.out.println(findById.iterator().next());
+    }
     
+    @Test
+    public void testFindByRate() {
+        Iterable<TaxRate> findByRate = taxRateDAO.findByRate(22);
+        findByRate.forEach(tr -> System.out.println(tr));
+    }
+    
+
     @Test
     public void testSave() {
-        
+
         Car car = new Car();
         car.setName("Testowy samochód");
-        
+
         carDAO.save(car);
-        
+
     }
-    
+
     @Test
     public void testFindByName() {
-        
+
         carDAO.findByNameLikeIgnoreCase("%t%");
-        
+
     }
-    
+
 }
