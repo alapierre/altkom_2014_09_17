f433868192fbd3061afc71c7f32a18e9bd76dc9d
Adrian Lapierre <adrian@soft-project.pl>
2014-09-19 11:58:39 +0200

    ws-client - auth - pobieranie zalogowanego usera

--- logistic-web/src/main/java/pl/com/softproject/sample/spring/ws/HelloServiceImpl.java
+++ logistic-web/src/main/java/pl/com/softproject/sample/spring/ws/HelloServiceImpl.java
@@ -4,7 +4,12 @@
 
 package pl.com.softproject.sample.spring.ws;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  *
@@ -13,9 +18,27 @@ import javax.jws.WebService;
 @WebService(endpointInterface = "pl.com.softproject.sample.spring.ws.HelloService")
 public class HelloServiceImpl implements HelloService {
     
+    @Resource
+    private WebServiceContext wscontext;
+    
     @Override
     public String sayHallo(String name) {
+        
+        System.out.println("wscontext = " + wscontext);
+        
+        System.out.println("logged user = " + wscontext.getUserPrincipal().getName()); 
+        
+        bySpringSecurity();
+        
         return "Hello " + name;
+    }
+    
+    private void bySpringSecurity() {
+        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName(); //get logged in username
+        
+        System.out.println("logged user by spring = " + name); 
     }
     
 }
