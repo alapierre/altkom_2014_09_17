782e69b9d56216e8922d87aab44b4ee5dac2a679
Adrian Lapierre <adrian@soft-project.pl>
2014-09-19 13:46:42 +0200

    scheduler

---
+++ logistic-web/src/main/java/pl/altkom/logistic/web/ReportServiceImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.altkom.logistic.web;
+
+/**
+ *
+ * @author Adrian Lapierre <adrian@soft-project.pl>
+ */
+public class ReportServiceImpl {
+    
+    public void sendReport() {
+        System.out.println("sending !!!");
+    }
+    
+}
